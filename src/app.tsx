5482545e256c915a8e4ce366d6deacf3f64e12cb
leohb07 <leohb1501@gmail.com>
2024-02-06 23:20:42 -0300

    feat: fill notes in props note card component

--- src/app.tsx
+++ src/app.tsx
@@ -20,9 +20,10 @@ export function App() {
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={{
+          content: "Hello World",
+          date: new Date()
+        }} />
       </div>
     </div>
   );
