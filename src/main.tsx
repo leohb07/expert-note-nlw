69a1a1e38747d697672ecbf78aaec1294d3466a4
leohb07 <leohb1501@gmail.com>
2024-02-07 22:25:16 -0300

    feat: add sonner

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "sonner"
+
 import { App } from "./app";
 
 import "./assets/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Toaster richColors />
     <App />
   </React.StrictMode>
 );
